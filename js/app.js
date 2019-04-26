f47034a60bfd350bbbd9a9f7830e266001ef5c43
S-Maitland <scott@maitland.me.uk>
2019-04-26 14:31:13 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+
+const form = document.querySelector('#new-item-form');
+
+const submitHandler = function(event){
+  event.preventDefault();
+  const title = event.target.title.value;
+  const developer = event.target.developer.value;
+  const category = event.target.category.value;
+
+  //creates new list item to game list to display entries.
+  newItem = document.createElement('p');
+  newItem.textContent = `${title} ${developer} ${category}`;
+
+  gameList = document.querySelector('#game-list');
+  gameList.appendChild(newItem);
+  document.querySelector('#new-item-form').reset();
+};
+  form.addEventListener('submit', submitHandler)
+
+  //On load document creates a delete all button
+  deleteButton = document.createElement('input');
+  deleteButton.type = 'button';
+  deleteButton.value = 'Delete All'
+  form.appendChild(deleteButton);
+
+  const handleDelete = function(){
+    // gameList.innerHTML = "";
+    while (gameList.firstChild) {
+      gameList.removeChild(gameList.firstChild);
+    }
+  };
+
+  deleteButton.addEventListener('click', handleDelete);
+
+})
